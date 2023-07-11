2cd76f3f985856177460827dcfede4940a55d815
Dindin Mahpudeen <mahpudeen@gmail.com>
2023-07-11 16:55:31 +0700

    add icon

--- src/navigation/MainNavigator.js
+++ src/navigation/MainNavigator.js
@@ -2,14 +2,31 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
 import HomeScreen from '../screens/HomeScreen/HomeScreen';
 import LogoutScreen from '../screens/LogoutScreen/LogoutScreen';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Tab = createBottomTabNavigator();
 
 const MainNavigator = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Logout" component={LogoutScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Logout"
+        component={LogoutScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="sign-out" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
